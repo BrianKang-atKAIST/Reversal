82755a6130808e99c9398a8fb00e5554f9aa2eb8
BrianKang <kbm03@kaist.ac.kr>
2021-08-23 00:03:27 +0900

    reversing predicting function predict_result function in Reversal class has built flawlessly. Also, Graphical work following every steps. However, I found some great idea, making condition-returning functions. It will make code much more short and readable. (But I think this commit message is f**king unreadable.

--- Reversal_embedded.py
+++ Reversal_embedded.py
@@ -3,7 +3,7 @@ import time
 
 # 클래스 정의
 class Reversal:
-  def __init__(self, blockpixel, Info_height):
+  def __init__(self, blockpixel, Info_height) -> None:
     '''Reversal 클래스의 init 함수'''
     self.board = {(x, y): 0 if (x+y)%2==1 else 1 for x in range(8) for y in range(8)}
     self.board[(3, 3)] += 10
@@ -18,158 +18,185 @@ class Reversal:
     self.moves = 4
     self.blackscore = 2
     self.whitescore = 2
+    # 마지막 수를 표현하기 위한 상수. 아직 수를 두지 않아 False 상태이다.
+    self.last_move_coord = False
+    # 마우스 위치에 두었을 때 뒤집히는 돌들을 표시하기 위한 상수
+    self.mouseon_coord = (0, 0)
+    self.predict_result_set = set()
 
-  def move(self, coord):
+  def move(self, coord) -> None:
     if self.turn == 'BLACK':
       self.board[coord] %= 10 
       self.board[coord] += 10 
     else:
       self.board[coord] %= 10 
       self.board[coord] += 20 
-    self.check_move(coord)
+    to_reverse_set = self.check_move(coord)
+    for blockcoord in to_reverse_set:
+      self.reverse_stone(blockcoord)
+    self.moves += 1
+    if self.turn == 'BLACK':
+      self.blackscore += 1
+    else:
+      self.whitescore += 1
+    if self.last_move_coord:
+      self.board[self.last_move_coord] -= 1000
+    self.last_move_coord = coord
+    self.board[self.last_move_coord] += 1000
 
-  def check_move(self, coord):
-    x = temp_x = coord[0]
-    y = temp_y = coord[1]
+  def check_move(self, coord) -> set:
+    x = coord[0]
+    y = coord[1]
     to_reverse_set = set()
     # 위로 검사
+    temp_x = x
+    temp_y = y
+    to_reverse_set_UP = set()
     while temp_y>0:
       temp_y -= 1
       if self.board[(x, temp_y)]//10==0:
         break
       elif self.board[(x, temp_y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_UP
         break
       else:
-        to_reverse_set.add((x, temp_y))
+        to_reverse_set_UP.add((x, temp_y))
     # 아래로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_DOWN = set()
     while temp_y<7:
       temp_y += 1
       if self.board[(x, temp_y)]//10==0:
         break
       elif self.board[(x, temp_y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_DOWN
         break
       else:
-        to_reverse_set.add((x, temp_y))
+        to_reverse_set_DOWN.add((x, temp_y))
     # 왼쪽으로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_LEFT = set()
     while temp_x>0:
       temp_x -= 1
       if self.board[(temp_x, y)]//10==0:
         break
       elif self.board[(temp_x, y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_LEFT
         break
       else:
-        to_reverse_set.add((temp_x, y))
+        to_reverse_set_LEFT.add((temp_x, y))
     # 오른쪽으로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_RIGHT = set()
     while temp_x<7:
       temp_x += 1
       if self.board[(temp_x, y)]//10==0:
         break
       elif self.board[(temp_x, y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_RIGHT
         break
       else:
-        to_reverse_set.add((temp_x, y))
+        to_reverse_set_RIGHT.add((temp_x, y))
     # 왼쪽 위로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_UPLEFT = set()
     while min(temp_x, temp_y)>0:
       temp_x -= 1
       temp_y -= 1
       if self.board[(temp_x, temp_y)]//10==0:
         break
       elif self.board[(temp_x, temp_y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_UPLEFT
         break
       else:
-        to_reverse_set.add((temp_x, temp_y))
+        to_reverse_set_UPLEFT.add((temp_x, temp_y))
     # 오른쪽 위로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_RIGHTUP = set()
     while temp_y>0 and temp_x<7:
       temp_x += 1
       temp_y -= 1
       if self.board[(temp_x, temp_y)]//10==0:
         break
       elif self.board[(temp_x, temp_y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_RIGHTUP
         break
       else:
-        to_reverse_set.add((temp_x, temp_y))
+        to_reverse_set_RIGHTUP.add((temp_x, temp_y))
     # 오른쪽 아래로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_DOWNRIGHT = set()
     while max(temp_x, temp_y)<7:
       temp_x += 1
       temp_y += 1
       if self.board[(temp_x, temp_y)]//10==0:
         break
       elif self.board[(temp_x, temp_y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_DOWNRIGHT
         break
       else:
-        to_reverse_set.add((temp_x, temp_y))
+        to_reverse_set_DOWNRIGHT.add((temp_x, temp_y))
     # 왼쪽 아래로 검사
     temp_x = x
     temp_y = y
-    to_reverse_set = set()
+    to_reverse_set_DOWNLEFT = set()
     while temp_y<7 and temp_x>0:
       temp_x -= 1
       temp_y += 1
       if self.board[(temp_x, temp_y)]//10==0:
         break
       elif self.board[(temp_x, temp_y)]//10==stone_dict[self.turn]:
-        for blockcoord in to_reverse_set:
-          self.reverse_stone(blockcoord)
+        to_reverse_set |= to_reverse_set_DOWNLEFT
         break
       else:
-        to_reverse_set.add((temp_x, temp_y))
+        to_reverse_set_DOWNLEFT.add((temp_x, temp_y))
 
-    self.moves += 1
-    if self.turn == 'BLACK':
-      self.blackscore += 1
-    else:
-      self.whitescore += 1
+    return to_reverse_set
 
-  def reverse_stone(self, coord):
+  def reverse_stone(self, coord) -> None:
     if self.turn == 'BLACK':
       self.board[coord] -= 10
       self.blackscore += 1
+      self.whitescore -= 1
     else:
       self.board[coord] += 10
       self.whitescore += 1
+      self.blackscore -= 1
 
-  def turn_change(self):
+  def predict_result(self, coord):
+    if self.mouseon_coord == coord:
+      return
+    else:
+      for predict_coord in self.predict_result_set:
+        self.board[predict_coord] -= 100
+      self.mouseon_coord = coord
+      self.predict_result_set = set()
+      if (self.board[self.mouseon_coord]%100)//10 != 0:
+        return
+      self.predict_result_set = self.check_move(coord)
+      for predict_coord in self.predict_result_set:
+        self.board[predict_coord] += 100
+
+  def coord_out_of_range(self, coord) -> bool:
+    if coord[0] < 0 or coord[1] < 0 or coord[0] > 7 or coord[1] > 7:
+      return False
+    if (self.board[coord]%100) // 10 != 0:
+      return False
+    return True
+    
+  def turn_change(self) -> None:
     if self.turn == 'BLACK':
       self.turn = 'WHITE'
     else:
       self.turn = 'BLACK'
 
-
-
 # 함수 정의
-
 
 # 상수 정의
 color_dict = {
@@ -179,9 +206,13 @@ color_dict = {
   'GREEN': (0, 255, 0),
   'BLUE': (0, 0, 255),
   'BROWN': (153, 102, 000),
-  'LT_BROWN': (204, 153, 102)
+  'LT_BROWN': (204, 153, 102),
+  'CREAMY_PURPLE': (204, 153, 255),
+  'PALE_YELLOW': (255, 204, 102),
+  'DIM_BLACK': (51, 51, 51),
+  'DIM_WHITE': (204, 204, 204)
 }
-stone_dict = {'BLACK': 1, 'WHITE': 2}
+stone_dict = {'BLACK': 1, 'WHITE': 2, 'DIM_BLACK': 3, 'DIM_WHITE': 4}
 Info_height = 150
 origin = (0, Info_height)
 blockpixel = 80
--- Reversal_gamescreen.py
+++ Reversal_gamescreen.py
@@ -17,10 +17,24 @@ def draw_background():
   pygame.draw.rect(screen, color_dict['GREEN'], [0, 0, Reversalgame.screen_width, Reversalgame.screen_height])
 
 def draw_stone(stone, coord, radius):
-    pygame.draw.circle(screen, color_dict[stone], coord, radius)
+  pygame.draw.circle(screen, color_dict[stone], coord, radius)
 
 def draw_block(color, coord, width, height):
-    pygame.draw.rect(screen, color_dict[color], [coord[0], coord[1], width, height])
+  pygame.draw.rect(screen, color_dict[color], [coord[0], coord[1], width, height])
+
+def draw_Word(font, word, color, center):
+  text = font.render(word, True, color)
+  text_rect =text.get_rect(center=center)
+  screen.blit(text, text_rect)
+
+def draw_Info():
+  blackscore = Reversalgame.blackscore
+  whitescore = Reversalgame.whitescore
+  turn = Reversalgame.turn
+  draw_Word(font_dict['BLACKSCORE'], str(blackscore), color_dict['RED'], (20, 30))
+  draw_Word(font_dict['WHITESCORE'], str(whitescore), color_dict['BLUE'], (620, 30))
+  draw_Word(font_dict['TURN'], str(turn)+"'s turn", color_dict[turn], (310, 30))
+
 
 def draw_board(origin=tuple):
   origin_x = origin[0]
@@ -28,46 +42,92 @@ def draw_board(origin=tuple):
   for (coord, info) in Reversalgame.board.items():
     block_x = origin_x + coord[0] * blockpixel
     block_y = origin_y + coord[1] * blockpixel
-    if (coord[0]+coord[1]) % 2 == 0:
+    if info%1000 // 100 == 1:
+      color = 'CREAMY_PURPLE'
+    elif info%10000 // 1000 == 1:
+      color = 'PALE_YELLOW'
+    elif (coord[0]+coord[1]) % 2 == 0:
       color = 'BROWN'
     else:
       color = 'LT_BROWN'
     draw_block(color, (block_x, block_y), blockpixel, blockpixel)
-    if info//10 == 1:
+    if (info%100)//10 == 1:
       stone = 'BLACK'
-    elif info//10 == 2:
+    elif (info%100)//10 == 2:
       stone = 'WHITE'
     else:
       continue
     circle_x = origin_x + (coord[0]+0.5) * blockpixel
     circle_y = origin_y + (coord[1]+0.5) * blockpixel
     draw_stone(stone, (circle_x, circle_y), 30)
+  if Reversalgame.coord_out_of_range(Reversalgame.mouseon_coord):
+    mouse_x = origin_x + (Reversalgame.mouseon_coord[0]+0.5) * blockpixel
+    mouse_y = origin_y + (Reversalgame.mouseon_coord[1]+0.5) * blockpixel
+    draw_stone('DIM_' + Reversalgame.turn, (mouse_x, mouse_y), 30)
+
+# 상수 정의
+font_dict = {
+  'BLACKSCORE': pygame.font.Font(None, 40),
+  'WHITESCORE': pygame.font.Font(None, 40),
+  'TURN': pygame.font.Font(None, 40),
+}
 
 # 이벤트 루프
-running = True # 게임이 진행중인가?
-while running:
-  for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가? 를 체크
-    if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
-      running = False # 게임이 진행중이 아님
+def main():
+  running = True # 게임이 진행중인가?
+  while running:
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가? 를 체크
+      if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
+        running = False # 게임이 진행중이 아님
 
-  draw_background()
-  draw_board(origin=origin)
-  # print(pygame.mouse.get_pressed())
+    draw_background()
+    # print(pygame.mouse.get_pressed())
 
-  if pygame.mouse.get_pressed() == (True, False, False):
-    click_x, click_y = pygame.mouse.get_pos()
-    click_x -= origin[0]
-    click_y -= origin[1]
-    click_x //= blockpixel
-    click_y //= blockpixel
-    if Reversalgame.board[(click_x, click_y)]//10 == 0:
-      Reversalgame.move((click_x, click_y))
-      Reversalgame.turn_change()
-    else:
-      print(Reversalgame.board[(click_x, click_y)])
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    mouse_x -= origin[0]
+    mouse_y -= origin[1]
+    mouse_x //= blockpixel
+    mouse_y //= blockpixel
+    try:
+      Reversalgame.predict_result((mouse_x, mouse_y))
+    except KeyError:
+      pass
+
+    if pygame.mouse.get_pressed() == (True, False, False):
+      click_x, click_y = pygame.mouse.get_pos()
+      click_x -= origin[0]
+      click_y -= origin[1]
+      click_x //= blockpixel
+      click_y //= blockpixel
+      try:
+        if (Reversalgame.board[(click_x, click_y)]%100)//10 == 0:
+          Reversalgame.move((click_x, click_y))
+          Reversalgame.turn_change()
+          # time.sleep(0.5)
+      except KeyError:
+        print('Out of board')
+        time.sleep(0.5)
+      else:
+        # print(Reversalgame.board[(click_x, click_y)])
+        pass
+
+    if Reversalgame.moves == 64:
+      if Reversalgame.blackscore > Reversalgame.whitescore:
+        print('BLACK WIN')
+      elif Reversalgame.blackscore < Reversalgame.whitescore:
+        print('WHITE WIN')
+      else:
+        print('DRAW GAME')
+      Reversalgame.moves = 0
+
+    draw_board(origin=origin)
+    draw_Info()
 
 
-  pygame.display.update()
+    pygame.display.update()
 
-# pygame 종료
-pygame.quit()
+  # pygame 종료
+  pygame.quit()
+
+if __name__== '__main__':
+  main()
